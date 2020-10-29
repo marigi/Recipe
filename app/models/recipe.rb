13591de8affd994fe380d3af3fe3f1014e193262
Marigi <marigindegwa@gmail.com>
2020-10-29 05:26:25 +0300

    1. Added validation for users when deleting and adding recipes 2. Added styling for the Login Button

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,8 @@ class Recipe < ApplicationRecord
   
   belongs_to :user
 
-  has_many :ingredients
-  has_many :directions
+  has_many :ingredients, :dependent => :destroy
+  has_many :directions, :dependent => :destroy
 
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
